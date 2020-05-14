e158e701c5f918aa5d51765d99ec4c894a5fb80c
Raja's <gkrajareddy@gmail.com>
2020-05-14 21:28:06 +0530

    Updates

---
+++ precache-manifest.afcb2782e404d95042af8fe37100b983.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686dda3774dc5dd346b0024c74455ad7",
+    "url": "/front-end-web-development-with-react-asignment-2/index.html"
+  },
+  {
+    "revision": "d980ee20f1ded5c11ba7",
+    "url": "/front-end-web-development-with-react-asignment-2/static/css/2.a856b994.chunk.css"
+  },
+  {
+    "revision": "8cc36ee7f1279b31c19b",
+    "url": "/front-end-web-development-with-react-asignment-2/static/css/main.bc3c158c.chunk.css"
+  },
+  {
+    "revision": "d980ee20f1ded5c11ba7",
+    "url": "/front-end-web-development-with-react-asignment-2/static/js/2.14951915.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/front-end-web-development-with-react-asignment-2/static/js/2.14951915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc36ee7f1279b31c19b",
+    "url": "/front-end-web-development-with-react-asignment-2/static/js/main.edd572a2.chunk.js"
+  },
+  {
+    "revision": "960bc953edb7099c7a11",
+    "url": "/front-end-web-development-with-react-asignment-2/static/js/runtime-main.0b152020.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/front-end-web-development-with-react-asignment-2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/front-end-web-development-with-react-asignment-2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/front-end-web-development-with-react-asignment-2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/front-end-web-development-with-react-asignment-2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/front-end-web-development-with-react-asignment-2/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
